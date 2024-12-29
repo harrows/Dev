0ce7b83b3d358edcfd3dab11f0f68cc921291b46
Дмитрий Машонский <harrows2012@gmail.com>
2024-12-29 07:26:25 +0300

    Change dir

---
+++ guess_number.py
@@ -0,0 +1,22 @@
+from random import randint
+
+number = randint(1, 100)
+print('!!!!Угадайте число от 1 до 100')
+
+def main():
+    while True:
+        quest = int(input('Введите свое число: '))
+
+        if quest < number:
+            print('Ваше число меньше загаданного')
+        
+        elif quest > number:
+            print('Ваше число больше загаданного')
+
+        elif quest == number:
+            break
+
+
+main()
+
+print('Вы угадали!!!!!')
