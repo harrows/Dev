2ba3e2fe58d2c88cf5088f9f57de97ff6c25303c
Дмитрий Машонский <harrows2012@gmail.com>
2025-01-14 16:00:48 +0300

    repeat learning

--- learning.py
+++ learning.py
@@ -1,27 +1,24 @@
 class Phone:
-
-    line_type = 'проводной'
+    line_type = 'Проводной'
 
     def __init__(self, dial_type_value):
         self.dial_type = dial_type_value
 
     def ring(self):
-        print('Дзззззыыыыыыыынь!')
-
+        print('Дзынь!!!!!!!!!!!!')
+    
     def call(self, phone_number):
         print(f'Звоню по номеру {phone_number}! Тип связи - {self.line_type}.')
 
-    # Ещё один метод.
-    def get_missed_calls(self):
-        print('Запрос количества пропущенных вызовов.')
-
+    def upgrade(self, line_type):
+        self.new_dial_type = line_type
+        print(f'тип связи {line_type}')
 
 rotary_phone = Phone(dial_type_value='дисковый')
 
+# rotary_phone.ring()
 
-print(rotary_phone.dial_type)
+rotary_phone.call('111-222')
+rotary_phone.upgrade()
 
-rotary_phone.dial_type = 'кнопочный'
-
-print(rotary_phone.dial_type)
-rotary_phone.get_missed_calls()
+rotary_phone.call('111-222')
