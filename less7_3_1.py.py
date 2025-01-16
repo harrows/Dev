d8cdc9586a464a06cff142573323bbee84b381a0
Дмитрий Машонский <harrows2012@gmail.com>
2025-01-16 15:46:36 +0300

    issue_third_quest

---
+++ less7_3_1.py.py
@@ -0,0 +1,49 @@
+class CipherMaster:
+    # Не изменяйте и не перемещайте эту переменную
+    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+    def cipher(self, original_text, shift):
+        # Метод должен возвращать зашифрованный текст
+        # с учетом переданного смещения shift.
+        result = []
+        for letter in original_text:
+            if letter.lower() not in CipherMaster.alphabet:
+                result.append(letter)
+            else:
+                tmp_index = CipherMaster.alphabet.index(letter.lower()) + shift
+                
+                if tmp_index >= len(CipherMaster.alphabet):
+                    tmp_index -= len(CipherMaster.alphabet)
+                elif tmp_index < 0:
+                    tmp_index += len(CipherMaster.alphabet)
+                
+                result.append(CipherMaster.alphabet[tmp_index])
+        return ''.join(result)
+
+    def decipher(self, cipher_text, shift):
+        # Метод должен возвращать исходный текст
+        # с учётом переданного смещения shift.
+        result = []
+        for letter in cipher_text:
+            if letter.lower() not in CipherMaster.alphabet:
+                result.append(letter)
+            else:
+                tmp_index = CipherMaster.alphabet.index(letter.lower()) - shift
+                
+                if tmp_index >= len(CipherMaster.alphabet):
+                    tmp_index -= len(CipherMaster.alphabet)
+                elif tmp_index < 0:
+                    tmp_index += len(CipherMaster.alphabet)
+                
+                result.append(CipherMaster.alphabet[tmp_index])
+        return ''.join(result)
+
+cipher_master = CipherMaster()
+print(cipher_master.cipher(
+    original_text='Однажды ревьюер принял проект с первого раза, с тех пор я его боюсь',
+    shift=2
+))
+print(cipher_master.decipher(
+    cipher_text='Олебэи яфвнэ мроплж сэжи — э пэй рдв злййвкпш лп нвящывнэ',
+    shift=-3
+))
