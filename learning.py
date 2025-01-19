1c134ddfa1e2bb4e41b4190b6af43da7587789fa
Дмитрий Машонский <harrows2012@gmail.com>
2025-01-20 02:04:20 +0330

    обучание с сеткой

--- learning.py
+++ learning.py
@@ -1,23 +1,11 @@
-class Phone:
-
-    line_type = 'проводной'
-
-    def __init__(self, dial_type_value):
-        self.dial_type = dial_type_value
-
-    def ring(self):
-        print('Дзынь!!!!')
+class Person():
+    def __init__(self, name, age, city):
+        self.name = name
+        self.age = age
+        self.city = city
     
-    def call(self, phone_number):
-        print(f'Звоню по номеру {phone_number}! Тип связи - {self.line_type}.')
-
-    def dial_type_upgrade(self, new_dial_type):
-        self.dial_type = new_dial_type
-
-    def __str__(self):
-        return (f'Это {self.line_type} телефон. Набор - {self.dial_type}.')
-
-rotary_phone = Phone(dial_type_value='дисковый')
-
-
-print(rotary_phone)
+    def greet(self):
+        return f'Привет, меня зовут {self.name}, мне {self.age} лет, я из города {self.city}.'
+    
+man = Person('Василий', 23, 'Москва')
+print(man.greet())
