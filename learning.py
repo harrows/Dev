7db2b563a22db07765ff6ab0c278b89cbee083b1
Дмитрий Машонский <harrows2012@gmail.com>
2025-01-21 16:09:35 +0300

    обучение

--- learning.py
+++ learning.py
@@ -1,46 +1,32 @@
-class CipherMaster:
-    # Не изменяйте и не перемещайте эту переменную
-    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+class Transport:
+    def __init__(self, speed):
+        self.speed = speed
 
-    def cipher(self, original_text, shift):
-        # Метод должен возвращать зашифрованный текст
-        # с учетом переданного смещения shift.
-        result = []
-        for letter in original_text:
-            if letter.lower() not in self.alphabet:
-                result.append(letter)
-            else:
-                tmp_index = self.alphabet.index(letter.lower()) + shift
-                if tmp_index >= len(self.alphabet):
-                    tmp_index -= len(self.alphabet)
-                result.append(self.alphabet[tmp_index])
-                      
-        
-        return ''.join(result)
+    def move(self):
+        return f'Этот транспорт движется'
 
-    def decipher(self, cipher_text, shift):
-        # Метод должен возвращать исходный текст
-        # с учётом переданного смещения shift.
-        result = []
-        for letter in cipher_text:
-            if letter.lower() not in self.alphabet:
-                result.append(letter)
-            else:
-                tmp_index = self.alphabet.index(letter.lower()) + shift
-                if tmp_index < 0:
-                    tmp_index += len(self.alphabet)
-                result.append(self.alphabet[tmp_index])
-        return ''.join(result)
+class Car(Transport):
+    def __init__(self, speed):
+        self.speed = speed
+
+    def move(self, speed):
+        return f'Машина едет по дороге со скоростью {speed} км/ч'
+    
+class Bike(Transport):
+    def move(self, speed):
+        return f'Велосипед едет по дороге со скоростью {speed} км/ч'
+    
+class Airplane(Transport):
+    def move(self):
+        return f'Самолет летит со скоростью {speed} км/ч'
+    
 
 
-cipher_master = CipherMaster()
-print(cipher_master.cipher(
-    original_text='Однажды ревьюер принял проект с первого раза, с тех пор я его боюсь',
-    shift=2
-))
-print(cipher_master.decipher(
-    cipher_text='Олебэи яфвнэ мроплж сэжи — э пэй рдв злййвкпш лп нвящывнэ',
-    shift=-3
-))
+car = Car()
+bike = Bike()
+air = Airplane()
 
+my_list = [car, bike, air]
 
+for i in my_list:
+    print(i.move())
