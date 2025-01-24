20d71a05cd2dd9f82c60d039a236b7077e7e7a6e
Дмитрий Машонский <harrows2012@gmail.com>
2025-01-24 16:30:32 +0300

    training

--- learning.py
+++ learning.py
@@ -1,32 +1,45 @@
-class Transport:
-    def __init__(self, speed):
-        self.speed = speed
+# class whell:
+#     def __init__(self):
+        
+#         self.whell = 'велосипеднге'
 
-    def move(self):
-        return f'Этот транспорт движется'
+# class Transport:
+#     def __init__(self, speed):
+#         self.speed = speed
+#         self.whell = whell()
 
-class Car(Transport):
-    def __init__(self, speed):
-        self.speed = speed
+#     def move(self):
+#         return f'Этот транспорт движется'
 
-    def move(self, speed):
-        return f'Машина едет по дороге со скоростью {speed} км/ч'
+# class Car(Transport):
+#     def __init__(self, speed):
+#         self.speed = speed
+
+#     def move(self, speed):
+#         return f'Машина едет по дороге со скоростью {speed} км/ч'
     
-class Bike(Transport):
-    def move(self, speed):
-        return f'Велосипед едет по дороге со скоростью {speed} км/ч'
+# class Bike(Transport):
+#     def move(self, speed):
+#         return f'Велосипед едет по дороге со скоростью {speed} км/ч'
     
-class Airplane(Transport):
-    def move(self):
-        return f'Самолет летит со скоростью {speed} км/ч'
+# class Airplane(Transport):
+#     def move(self):
+#         return f'Самолет летит со скоростью {speed} км/ч'
     
 
 
-car = Car()
-bike = Bike()
-air = Airplane()
+# car = Car()
+# bike = Bike()
+# air = Airplane()
 
-my_list = [car, bike, air]
+# my_list = [car, bike, air]
 
-for i in my_list:
-    print(i.move())
+# for i in my_list:
+#     print(i.move())
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+t = ((b - a) / c)
+print()
