417b4dfb78f2fbf612e0f900c3a9a18e653159a6
Дмитрий Машонский <harrows2012@gmail.com>
2025-01-28 23:56:21 +0300

    learning module

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -1,7 +1,20 @@
 from gameparts import Board
 
+# def main():
+#     game = Board()
+#     game.display()
+#     game.make_move(1, 1, 'X')
+#     print('Ход сделан!')
+#     game.display() 
+
+# if __name__ == '__main__':
+#     main()
+
+
 game = Board()
 game.display()
 game.make_move(1, 1, 'X')
 print('Ход сделан!')
 game.display() 
+
+print(dir(game))
