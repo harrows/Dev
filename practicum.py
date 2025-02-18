bfce6be79ca52464c9adfab35ea3b36622d6cbf2
Дмитрий Машонский <harrows2012@gmail.com>
2025-02-18 16:30:17 +0300

    decorators

--- practicum.py
+++ practicum.py
@@ -1,2 +1,47 @@
-def say_hello():
-    print("Hello!")
+from random import choice, uniform
+
+
+def format_float_return(func):
+    def wrapper():
+        result = func()
+        if type(result) == float:
+            return f'{result:.2f}'
+        else:
+            return result
+    
+    return wrapper
+        
+
+    # Ваш код здесь
+
+
+# Не изменяйте код ниже: он поможет проверить работу декоратора.
+# Декорируем функцию:
+@format_float_return
+def test_function_1():
+    """Возвращает случайное число типа float в диапазоне от -10 до 10,
+    например -4.3897268052813265.
+    """
+    return uniform(-10, 10)
+
+
+# Декорируем вторую функцию:
+@format_float_return
+def test_function_2():
+    """Возвращает случайный элемент списка sequence - число или строку."""
+    sequence = [
+        3.1415926535,
+        'pi',
+        3.14,
+        'пи',
+        'три целых четырнадцать сотых',
+        3.14159
+    ]
+    # Функция choice() из модуля random возвращает 
+    # случайный элемент последовательности.
+    return choice(sequence)
+
+
+# Вызовем задекорированные функции для проверки работы декоратора:
+print(test_function_1())
+print(test_function_2())
